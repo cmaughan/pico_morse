83c792966ac49ca476fd050df9ce58685ed18977
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2025-04-04 11:53:43 +0100

    VBand

--- code.py
+++ code.py
@@ -1,29 +1,27 @@
-
 # SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
 
-"""CircuitPython Essentials HID Keyboard example"""
+"""CircuitPython Essentials HID Keyboard example - Multi-key support"""
 import time
 
 import board
 import digitalio
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 
-# A simple neat keyboard demo in CircuitPython
+# A simple neat keyboard demo in CircuitPython with multi-key support
 
 # The pins we'll use, each will have an internal pullup
 keypress_pins = [board.GP0, board.GP1]
 key_pin_array = []
-keys_pressed = [Keycode.A, Keycode.S]
+keys_pressed = [Keycode.RIGHT_BRACKET, Keycode.LEFT_BRACKET]
+key_states = [False, False]  # Track the state of each key (pressed/released)
 
 # The keyboard object!
 time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
 keyboard = Keyboard(usb_hid.devices)
-keyboard_layout = KeyboardLayoutUS(keyboard)  # We're in the US :)
 
 # Make all pin objects inputs with pullups
 for pin in keypress_pins:
@@ -41,25 +39,21 @@ led.direction = digitalio.Direction.OUTPUT
 print("Waiting for key pin...")
 led.value = False
 
-#keyboard.press(Keycode.A)  # ...Print the string
 while True:
-    # Check each pin
-    for key_pin in key_pin_array:
-        if not key_pin.value:  # Is it grounded?
-            i = key_pin_array.index(key_pin)
-            print("Pin #%d is grounded." % i)
+    for i, key_pin in enumerate(key_pin_array):
+        if not key_pin.value:  # Key is pressed
+            if not key_states[i]:  # If it was not already pressed
+                print(f"Key {keys_pressed[i]} pressed.")
+                keyboard.press(keys_pressed[i])
+                key_states[i] = True  # Update state to pressed
+                led.value = True  # Turn on LED
+        else:  # Key is released
+            if key_states[i]:  # If it was previously pressed
+                print(f"Key {keys_pressed[i]} released.")
+                keyboard.release(keys_pressed[i])
+                key_states[i] = False  # Update state to released
 
-            keyboard.press(keys_pressed[i])
+    # Turn off LED if no keys are pressed
+    led.value = any(key_states)
 
-            # Turn on the red LED
-            led.value = True
-
-            while not key_pin.value:
-                pass  # Wait for it to be ungrounded!
-
-            keyboard.release_all()  # ..."Release"!
-
-            # Turn off the red LED
-            led.value = False
-
-    time.sleep(0.01)
+    time.sleep(0.0001)
